4f51450c606ff52fb73dc52eb24146d769fd2ced
Phai <norapat_c@cmu.ac.th>
2023-08-07 15:06:33 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Norapat Chindasoon"
+            studentId="650610774"
           />
         </div>
       </body>
